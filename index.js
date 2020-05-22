d078d6ba0028315a9f8d1bb82c1d534b12e19929
vineet30222 <vineet302222@gmail.com>
2020-05-22 15:01:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express= require('express');
+const app = express();
+const port = 8000;
+
+app.listen(port,function(err)
+{
+    if(err)
+    {
+        //interpolation
+        console.log(`Error in running the server:${err}`);
+    }
+
+    console.log(`Server is Running on port ${port}`);
+})
